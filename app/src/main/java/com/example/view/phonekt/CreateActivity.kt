32db730eeca20d4f086d63e8fdef57360c326ce0
Choi YoonChan <choyc9239@naver.com>
2022-02-27 16:20:42 +0900

    first

---
+++ app/src/main/java/com/example/view/phonekt/CreateActivity.kt
@@ -0,0 +1,18 @@
+package com.example.view.phonekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.view.phonekt.databinding.ActivityCreateBinding
+
+class CreateActivity : AppCompatActivity() {
+
+
+    private lateinit var cBinding: ActivityCreateBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_create)
+
+
+    }
+}
